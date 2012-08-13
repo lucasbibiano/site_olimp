b21618d65928b1d545d399015a0940dc2baed4eb
painkiller778 <painkiller778@gmail.com>
2012-08-13 20:30:29 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteOlimpiadas::Application.config.secret_token = '8458b261fc3ca71ab61a8fd4a523e52c10e563f707d1a121d44a3e805198d427015f9afd388c01f07e3cccd0f82d77b72532f9fb0106d0469020d8dac8273b05'
